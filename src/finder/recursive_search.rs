3cee438c5b2f57c5e6dfb27c41f6ef23adee6646
uff20xd <donut121008@gmail.com>
2025-06-02 18:59:36 +0200

    k

--- src/finder/recursive_search.rs
+++ src/finder/recursive_search.rs
@@ -10,33 +10,48 @@ use crate::error::SaltzError;
 
 struct Project (String, PathBuf);
 
+pub struct Projects(Vec<Project>);
+
+impl Projects {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+    pub fn get_files (&mut self) -> () {
+    }
+    fn format (paths: Vec<PathBuf>) -> Result<Vec<Project>, SaltzError> {
+        let mut projects: Vec<Project> = Vec::new();
+        for i in paths {
+        }
+        Ok(projects)
+    }
+    pub fn save_paths(&mut self) -> () {
+        //opens local file and writes all projects and paths into it
+    }
+    pub fn get_paths(&mut self) -> (){
+        //get all the files and paths from the "database"
+    }
+}
 fn search_directory (path: PathBuf, hidden_files: bool) -> Vec<PathBuf> {
-    let mut directories = Vec::new();
-    let mut list_command = Command::new("ls");
-    let raw_output = list_command.output().expect("This command should work usually");
-    //let raw_string_of_files = str::from_utf8(&output.stdout[0..output.stdout.len()]);
-    let output = raw_output.stdout;
+    let mut directories: Vec<PathBuf> = Vec::new();
+    let mut list_command: Command = Command::new("ls");
+    let raw_output: Output = list_command.output().expect("This command should work usually");
+    let output: Vec<u8> = raw_output.stdout;
     let mut slice_start: usize = 0;
     let mut slice_end: usize = 0;
+    let mut files: Vec<String> = Vec::new();
     while output.len()-1 >= slice_end {
         slice_end += 1;
         let _  = match output[slice_end] {
-            b' ' => (),
+            b' ' => {
+                let file = str::from_utf8(&output[slice_start..slice_end]).expect("should be a string in string in search directory").to_owned();
+                files.push(file);
+                slice_start = slice_end;
+            },
             _ => ()
         };
     }
 
     directories
-}
-pub fn get_files () -> () {
-    ()
-}
-fn format (paths: Vec<PathBuf>) -> Result<Vec<Project>, SaltzError> {
-    let mut projects = Vec::new();
-    for i in paths {
-
-    }
-    Ok(projects)
 }
 pub fn get_home_directory () -> String {
     let current_user_as_osstring = get_current_username()
@@ -45,3 +60,6 @@ pub fn get_home_directory () -> String {
         .expect("couldnt convert username to a string");
     ("/home/").to_string() + current_user
 }
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 mod finder;
 mod error;
 use std::{
-    str,
-    process::{
-        Command
-    },
+    env::{current_exe, set_current_dir}, process::Command, str
 };
 use users::*;
 use finder::recursive_search::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+    //let test = set_current_dir("");
+    println!("{:?}", current_exe());
+    loop {
+        print!("a");
+    }
     Ok(())
 }
